17024d4a06d15b69bfa215171d3fe074920fcd96
Josue Alfonzo <16-00436@usb.ve>
2022-05-13 14:00:52 -0400

    ejercicios

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,6 @@
+import MediaPlayer from "./MediaPlayer.mjs";
 const video = document.querySelector('video');
-const button = document.querySelector('button');
-
-function MediaPlayer(config){
-    this.media = config.el
-}
-
-MediaPlayer.prototype.play = function(){
-    this.media.play();
-};
-
-MediaPlayer.prototype.pause = function(){
-  this.media.pause();
-}
-
-MediaPlayer.prototype.togglePlay = function(){
-  if(this.media.paused){
-    this.play();
-  }else {
-    this.pause();
-  }
-};
-
-
-
 const player = new MediaPlayer({el: video});
-
+const button = document.querySelector('button');
 button.onclick = () => player.togglePlay();
+
